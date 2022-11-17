8bf130b36db4a846adebaf72d8ff245a8e4f5b0d
Daryle Posadas <posadas2@uic.edu>
2022-11-17 17:25:05 +0000

    amazhe3_posadas2

--- collab.c
+++ collab.c
@@ -21,11 +21,18 @@ void reed_ekidan2() {
   printf("We learned that git and GitHub are amazing, powerful tools!\n");
 }
 
+void amazhe3_posadas2() {
+  printf("Azka Mazher and Daryle Posadas");
+  printf("At first we had trouble cloning Kidane's repo. Initially, we tried importing it through replit without the commands and it wasn't working. Then we tried using the url directly from the github site and it wasn't working because it wasn't the exact URL for the repository. Finally, we tried the URL given in the lab description and it worked!");
+  printf("Another issue we ran into was trying to add our code to allow for it to be committed. What we did wrong was trying to add the file without being in the correct directory, so once we were in the right directory, our file was added no problem.");
+}
+
 int main()
 {
     printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
     
     reed_ekidan2();
+    amazhe3_posadas2();
     
     return 0;
 }
